4ddacc675e29095ef9fa95155bdb744714de2626
Paul Thorsen <paul_thorsen@byu.edu>
2021-03-23 13:23:19 -0600

    refactor + add HOH and BR

--- sportsMail.py
+++ sportsMail.py
@@ -22,10 +22,12 @@ def getCurrentDay():
 
 
 # Might redo this one day to make it more readable but this was more fun, I guess...
-def getTop5VideoTweetsOfToday(tweets: []):
+def getTop5VideoTweetsOfToday(tweets):
     """
     Create (tweet, mediaExp) tuples that are sorted (descending order) by view count - top 5 most viewed video tweets are returned as [(tweet, mediaExp)...]
     """
+    if len(tweets) == 0:
+        return []
     tweets = sorted(
         [
             (
@@ -51,6 +53,55 @@ def getTop5VideoTweetsOfToday(tweets: []):
     return tweets
 
 
+def fetchTweetsFrom(handle, url, headers):
+    resp = requests.get(
+        url.format(handle, getCurrentDay()),
+        headers=headers,
+    )
+    if resp.status_code != 200:
+        # probably should add some better error handling
+        print(resp.text)
+        exit
+    return json.loads(resp.text)
+
+
+def sendEmails(port, sender_email, password, recipients, email_subject):
+    # Create SMTP session for sending the mail
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as session:
+        session.login(sender_email, password)
+        #  Create new email for each recipient
+        for recipient in recipients:
+            # Create the email head (sender and subject)
+            email = MIMEMultipart("alternative")
+            email["From"] = sender_email
+            email[
+                "Subject"
+            ] = f'{email_subject} {datetime.today().strftime("%B %d, %Y")}'
+            # Add body to email
+            email.attach(MIMEText(body, "plain"))
+            email.attach(MIMEText(html_body, "html"))
+            email["To"] = recipient
+            session.sendmail(sender_email, recipient, email.as_string())
+            print("Mail sent successfully to " + recipient)
+        session.quit()
+
+
+def parseResp(resp, tweets, media):
+    if "errors" in resp.keys():
+        print(resp["errors"][0]["message"])
+        return
+    else:
+        meta = resp["meta"]
+        if meta["result_count"] > 0:
+            tweets = resp["data"]
+            if "includes" in resp.keys() and "media" in resp["includes"].keys():
+                media = resp["includes"]["media"]
+                return (tweets, media)
+    return ([], [])
+
+
 # Load ENV vars
 load_dotenv()
 
@@ -58,7 +109,6 @@ BEARER_TOKEN = os.environ.get("TWITTER_API_BEARER_TOKEN")
 SENDER_EMAIL = os.environ.get("SENDER_EMAIL")
 EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 RECIPIENTS = str(os.environ.get("RECIPIENTS")).split("...")
-ACCOUNT_HANDLE = "espn"
 TWITTER_API_URI = "https://api.twitter.com/2/tweets/search/recent?query=from:{}&start_time={}&max_results=100&tweet.fields=public_metrics,created_at,attachments,author_id&expansions=attachments.media_keys&media.fields=preview_image_url,type,duration_ms,width,public_metrics"
 HEADERS_DICT = {"Authorization": "Bearer " + BEARER_TOKEN}
 
@@ -70,35 +120,29 @@ MAX_WIDTH = "600px"
 EMAIL_SUBJECT = "Daily Sports Brief"
 PORT = 465  # For SSL
 
-# Fetch tweets
-resp = requests.get(
-    TWITTER_API_URI.format(ACCOUNT_HANDLE, getCurrentDay()),
-    headers=HEADERS_DICT,
-)
-if resp.status_code != 200:
-    # probably should add some error handling
-    print(resp.text)
-    exit
-else:
-    resp = json.loads(resp.text)
-    tweets = resp["data"]
-    if resp["includes"]["media"]:
-        media = resp["includes"]["media"]
-    else:
-        media = []
-    meta = resp["meta"]
+ACCOUNT_HANDLES = ["espn", "bleacherreport", "HofHighlights"]
 
-    top5 = getTop5VideoTweetsOfToday(tweets)
+top5 = []
+tweets = []
+media = []
+
+for handle in ACCOUNT_HANDLES:
+    resp = fetchTweetsFrom(handle, TWITTER_API_URI, HEADERS_DICT)
+    tweetsArr, mediaArr = parseResp(resp, tweets, media)
+    tweets += tweetsArr
+    media += mediaArr
+
+top5 = getTop5VideoTweetsOfToday(tweets)
 
 body = "Today's Top 5 ESPN Video Tweets\n\n"
 html_body = f"""
     <html>
         <body style="color: #333; max-width: {MAX_WIDTH};">
-            <h1 style="line-height: 40px;">Today's Top 5 ESPN Video Tweets</h1>
+            <h1 style="line-height: 40px;">Today's Top 5 Sports Video Tweets</h1>
     """
 
 if len(top5) == 0:
-    html_body += f"<p>{ACCOUNT_HANDLE} didn't post any videos today ¯\_(ツ)_/¯</p>"
+    html_body += "<p>No cool videos were posted today ¯\_(ツ)_/¯</p>"
 
 # Add tweets to body text
 for i, tweet in enumerate(top5):
@@ -126,21 +170,4 @@ html_body += """
     </html>
     """
 
-# Create SMTP session for sending the mail
-# Create a secure SSL context
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", PORT, context=context) as session:
-    session.login(SENDER_EMAIL, EMAIL_PASSWORD)
-    #  Create new email for each recipient
-    for recipient in RECIPIENTS:
-        # Create the email head (sender and subject)
-        email = MIMEMultipart("alternative")
-        email["From"] = SENDER_EMAIL
-        email["Subject"] = f'{EMAIL_SUBJECT} {datetime.today().strftime("%B %d, %Y")}'
-        # Add body to email
-        email.attach(MIMEText(body, "plain"))
-        email.attach(MIMEText(html_body, "html"))
-        email["To"] = recipient
-        session.sendmail(SENDER_EMAIL, recipient, email.as_string())
-    session.quit()
-# END
+sendEmails(PORT, SENDER_EMAIL, EMAIL_PASSWORD, RECIPIENTS, EMAIL_SUBJECT)
